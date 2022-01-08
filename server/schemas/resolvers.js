52b45d9f5c8a85c20430c4e5fa854d28a218ad7a
Jose Mejia <mejialaguna@yahoo.com>
2022-01-07 19:26:31 -0500

    Auth

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,101 @@
-const { User , Post } = require("../models")
+const { User, Post } = require("../models");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { UserInputError } = require("apollo-server");
+const { validateRegisterInput , validateLoginInput } = require("../utils/validate");
+
+const { secret } = require("../config/connection");
+
+function gToken (user) {
+  return jwt.sign(
+        {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+        },
+        secret,
+        { expiresIn: "1h" }
+      );
+}
 
 const resolvers = {
   Query: {
-    sayHi: () => "hello sexy papi",
+    async getPost() {
+      try {
+        const post = await Post.find();
+        return post;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+  Mutation: {
+    async login(parents, { username, password }) {
+      const { valid, errors } = validateLoginInput(username, password)
+      if (!valid) {
+        
+      }
+      const user = await User.findOne({ username })
+      if (!user) {
+        errors.general = "User doesnt exit";
+        throw new UserInputError("Errors", { errors });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = "wrong password";
+        throw new UserInputError("wrong password", { errors });
+      }
+      const token = gToken(user)
+      
+       return {
+         ...user._doc,
+         id: user._id,
+         token,
+       };
+    },
+    async register(
+      parents,
+      { registerInput: { username, email, password, confirmPassword } },
+      context,
+      info
+    ) {
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+
+      const user = await User.findOne({ username });
+      if (username) {
+        throw new UserInputError("username already exist", {
+          errors: {
+            username: " username already exist",
+          },
+        });
+      }
+      password = await bcrypt.hash(password, 12);
+
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      });
+
+      const res = await newUser.save();
+
+      const token = gToken(res);
+
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
+    },
   },
 };
-module.exports = resolvers
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,34 @@
 const { gql } = require("graphql-tag");
 
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    username: String!
+    createdAt: String!
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    token: String!
+    username: String!
+    createdAt: String!
+  }
+
+  input RegisterInput {
+    username: String!
+    password: String!
+    confirmPassword: String!
+    email: String!
+  }
+
   type Query {
-    sayHi: String!
+    getPost: [Post]
+  }
+  type Mutation {
+    register(registerInput: RegisterInput):User!
+    login(username: String!, password:  String!): User!
   }
 `;
 
