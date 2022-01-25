59c03317b6eb48226477fc5ae09632db6fcd3eda
Jose Mejia <mejialaguna@yahoo.com>
2022-01-24 21:17:08 -0500

    deploy-test

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require("apollo-server-express");
 const { PubSub } = require("graphql-subscriptions");
 // const { authMiddleware } = require("./utils/auth");
 const path = require("path");
-
+ 
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3002;
